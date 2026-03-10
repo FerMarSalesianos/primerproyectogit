b0544e33b31749359a9482ef16479ff603feb4b0
Fernando Marrero <fernando.marrero@salesianos-lacuesta.net>
2026-03-10 13:27:08 +0000

    mensaje con los cambios

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import net.salesianos.persona.Persona;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("hola clase de 1º dam");
     }
+    Persona p1 = new Persona("fernando", "gomez", 22);
 }
