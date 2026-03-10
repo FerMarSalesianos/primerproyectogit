b9edcc2ab4d43be8cc74a5466650acbb091f142c
Fernando Marrero <fernando.marrero@salesianos-lacuesta.net>
2026-03-10 13:14:24 +0000

    prueba credenciases

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("hola clase de dam");
+        System.out.println("hola clase de 1º dam");
     }
 }
